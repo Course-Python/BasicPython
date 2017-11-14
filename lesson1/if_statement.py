ad8fce7e6d2810520abba16a17b12e425f851eb9
Anton Adamiv <detonavomek@gmail.com>
2017-11-15 00:42:47 +0200

    add second lesson

---
+++ lesson1/if_statement.py
@@ -0,0 +1,8 @@
+age = 15
+
+if age > 50:
+    print(">50")
+else:
+    print("<50")
+
+print(">50" if age > 50 else "<50")
---
+++ lesson2/loop.py
@@ -0,0 +1,21 @@
+a = [1, 2, 3, 4]
+
+sum_of_elements = 0
+
+for element in a:
+	if element == 3:
+		# print('This element is 3. Prev sum: {}'.format(sum_of_elements))
+		break
+	sum_of_elements = sum_of_elements + element
+else:
+	print('Non break')
+
+print(sum_of_elements)
+
+# i = 0
+
+# while True:
+# 	print(i)
+# 	i = i + 1
+# 	if i > 5:
+# 		break
